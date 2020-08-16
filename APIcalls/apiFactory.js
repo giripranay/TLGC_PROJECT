35cf708d32d3ae893a7aa47518d3177ca3f342de
Giri Pranay K| Dvara Solutions <giripranay.k@Sagars-MacBook-Air.local>
2020-08-16 21:15:11 +0530

    Make bearer token a variable

--- APIcalls/apiFactory.js
+++ APIcalls/apiFactory.js
@@ -1,6 +1,7 @@
 angular.module('tlgcApp').factory('apiFactory', function($http){
 
-    var endpoint = 'https://api.github.com/repos/facebook/react/forks';    // Get users who forked the repo
+    const endpoint = 'https://api.github.com/repos/facebook/react/forks';    // Get users who forked the repo
+    const token = 'Bearer c8f2feab98e530bb1d44626b6f65183fc876f5df';   //Github token of the user , it helps in authentication.
 
     var apis = {};
 
@@ -13,7 +14,7 @@ angular.module('tlgcApp').factory('apiFactory', function($http){
             method: 'PUT',
             url: 'https://api.github.com/user/following/'+userName,
             headers: {
-               'Authorization': 'Bearer c8f2feab98e530bb1d44626b6f65183fc876f5df'   //Token value of the user who wants to follow.
+               'Authorization': token
             },
         }
         $http(req)
